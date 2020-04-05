84cb9ea8f4d6874a89f21e02a0a9ad6c84d14883
Soumya sankar dutta <saymesomu@gmail.com>
2020-04-06 01:28:43 +0530

    Updates

---
+++ precache-manifest.d96bb8ca80d6a97df9d253ce077be858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cc7168583fe1a0d7f5352ae5668e22",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7d8b65bd94a1c4b32a47",
+    "url": "/monsters-rolodex/static/css/main.1ce88572.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8b65bd94a1c4b32a47",
+    "url": "/monsters-rolodex/static/js/main.1f19cec3.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
